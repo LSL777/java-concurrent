390249e4b66f34b5281b0a5ab27272fc18da6710
LinShanglei <911930672@qq.com>
2025-07-24 11:42:16 +0800

    初始化项目

---
+++ src/main/java/com/linprogramming/Main.java
@@ -0,0 +1,10 @@
+package com.linprogramming;
+
+/**
+ * @author LinShanglei
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
